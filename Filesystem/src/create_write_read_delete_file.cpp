38bf51006ded4f2f13d7fbeb0cec3fb346e4cb47
RazviOverflow <razvioverflow@gmail.com>
2023-05-08 06:51:57 -0700

    Update create_write_read_delete_file.cpp

--- Filesystem/src/create_write_read_delete_file.cpp
+++ Filesystem/src/create_write_read_delete_file.cpp
@@ -3,30 +3,6 @@
 #include <iostream>
 #include <windows.h>
 
-std::string GetLastErrorAsString()
-{
-    //Get the error message ID, if any.
-    DWORD errorMessageID = ::GetLastError();
-    if(errorMessageID == 0) {
-        return std::string(); //No error message has been recorded
-    }
-    
-    LPSTR messageBuffer = nullptr;
-
-    //Ask Win32 to give us the string version of that message ID.
-    //The parameters we pass in, tell Win32 to create the buffer that holds the message for us (because we don't yet know how long the message string will be).
-    size_t size = FormatMessageA(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
-                                 NULL, errorMessageID, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPSTR)&messageBuffer, 0, NULL);
-    
-    //Copy the error message into a std::string.
-    std::string message(messageBuffer, size);
-    
-    //Free the Win32's string's buffer.
-    LocalFree(messageBuffer);
-            
-    return message;
-}
-
 int main() {
     // Create a new file named "example.txt"
     HANDLE fileHandle = CreateFile("example.txt", GENERIC_WRITE | GENERIC_READ, FILE_SHARE_READ | FILE_SHARE_WRITE | FILE_SHARE_DELETE, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
@@ -54,7 +30,6 @@ int main() {
     success = ReadFile(fileHandle, buffer, sizeof(buffer), &bytesRead, NULL);
     if (!success) {
         std::cout << "Failed to read from file." << std::endl;
-        std::cout << GetLastErrorAsString() << std::endl;
         CloseHandle(fileHandle);
         return 1;
     }
