7171d4588d0ba30afedb301a3f650dbd470764a8
RazviOverflow <razvioverflow@gmail.com>
2023-05-23 13:59:46 +0200

    Added mutex behaviors

---
+++ Process/src/check_mutex_v1.cpp
@@ -0,0 +1,21 @@
+// Compiled with: cl /EHsc .\check_mutex_v1.cpp
+// Check mutex using OpenMutex
+#include <Windows.h>
+#include <iostream>
+
+int main() {
+    // Check if the global mutex exists
+    HANDLE hMutex = OpenMutex(MUTEX_ALL_ACCESS, FALSE, "Global\\MyGlobalMutexExample");
+
+    if (hMutex == NULL) {
+        std::cout << "Global mutex does not exist." << std::endl;
+        return 1;
+    }
+
+    std::cout << "Global mutex exists." << std::endl;
+
+    // Close the handle
+    CloseHandle(hMutex);
+
+    return 0;
+}
---
+++ Process/src/create_mutex_v1.cpp
@@ -0,0 +1,28 @@
+// Compiled with: cl /EHsc .\create_mutex_v1.cpp
+// Create mutex using CreateMutex
+#include <Windows.h>
+#include <iostream>
+
+int main() {
+    // Create a global mutex
+    HANDLE hMutex = CreateMutex(NULL, FALSE, "Global\\MyGlobalMutexExample");
+
+    if (hMutex == NULL) {
+        std::cout << "Failed to create mutex. Error code: " << GetLastError() << std::endl;
+        return 1;
+    }
+
+    if (GetLastError() == ERROR_ALREADY_EXISTS) {
+        std::cout << "Mutex already exists." << std::endl;
+        return 1;
+    }
+
+    std::cout << "Mutex created successfully." << std::endl;
+
+    // Use the mutex...
+
+    // Release the mutex
+    CloseHandle(hMutex);
+
+    return 0;
+}
