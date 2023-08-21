02a9e8e5561ef44f9e69b6f95fb0e10d5d5dd6d7
RazviOverflow <razvioverflow@gmail.com>
2023-08-21 21:17:16 +0200

    Added src and exe for malloc

---
+++ [OC0002] Memory/[C0007] Allocate Memory/[C0007.005X] malloc/src/malloc.cpp
@@ -0,0 +1,20 @@
+// Compiled with: cl /EHsc .\malloc.cpp
+// Memory Allocation with malloc
+#include <Windows.h>
+#include <iostream>
+
+int main() {
+    size_t size = 4096;  // Size of memory to allocate (in bytes)
+    LPVOID lpMemory = malloc(size);  // Allocate memory using malloc
+
+    if (lpMemory == NULL) {
+        std::cerr << "malloc failed." << std::endl;
+        return 1;
+    }
+
+    // Use the allocated memory here...
+
+    //free(lpMemory);  // Free the allocated memory
+
+    return 0;
+}
