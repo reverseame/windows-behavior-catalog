82e4077428886e2d99fcdec6bab767cdaef508ba
RazviOverflow <razvioverflow@gmail.com>
2023-08-22 21:49:53 +0200

    Added src for VirtualProtect

---
+++ [OC0002] Memory/[C0008] Change Memory Protection/[C0008.001X] VirtualProtect/src/VirtualProtect.cpp
@@ -0,0 +1,33 @@
+// Compiled with: cl /EHsc .\VirtualPRotect.cpp
+// Memory protection modification with VirtualProtect
+#include <Windows.h>
+#include <iostream>
+
+int main() {
+    LPVOID lpMemory = VirtualAlloc(NULL, 4096, MEM_COMMIT, PAGE_READWRITE);  // Allocate memory with read-write protection
+
+    if (lpMemory == NULL) {
+        std::cerr << "VirtualAlloc failed." << std::endl;
+        return 1;
+    }
+
+    // Change the memory protection to PAGE_EXECUTE_READ
+    DWORD oldProtect;
+    if (!VirtualProtect(lpMemory, 4096, PAGE_EXECUTE_READ, &oldProtect)) {
+        std::cerr << "VirtualProtect failed." << std::endl;
+        VirtualFree(lpMemory, 0, MEM_RELEASE);  // Free the allocated memory
+        return 1;
+    }
+
+    // Use the memory with new protection...
+
+    // Restore the original protection
+    // DWORD restoredProtect;
+    // if (!VirtualProtect(lpMemory, 4096, oldProtect, &restoredProtect)) {
+    //     std::cerr << "VirtualProtect failed." << std::endl;
+    // }
+
+    // VirtualFree(lpMemory, 0, MEM_RELEASE);  // Free the allocated memory
+
+    return 0;
+}
