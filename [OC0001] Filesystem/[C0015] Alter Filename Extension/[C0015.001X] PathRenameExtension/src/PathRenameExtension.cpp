034f1f25126edc01ceccb8e2b5c7974d8ec37772
RazviOverflow <razvioverflow@gmail.com>
2023-08-23 18:14:37 +0200

    Added src and exe for PathRenameExtension

---
+++ [OC0001] Filesystem/[C0015] Alter Filename Extension/[C0015.001X] PathRenameExtension/src/PathRenameExtension.cpp
@@ -0,0 +1,20 @@
+// Compiled with: cl /EHsc .\PathRenameExtension.cpp shlwapi.lib
+// Alter filename extension with PathRenameExtension
+#include <windows.h>
+#include <shlwapi.h>
+#include <iostream>
+
+int main() {
+    // ANSI string
+    const char ansiFilePath[] = "C:\\path\\to\\file.txt";
+
+    // Change the file extension to ".dat"
+    char modifiedPath[MAX_PATH];
+    strcpy_s(modifiedPath, ansiFilePath);
+    PathRenameExtension(modifiedPath, ".dat");
+
+    // Print the modified file path
+    std::cout << "Modified file path: " << modifiedPath;
+
+    return 0;
+}
