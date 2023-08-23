07c8847c872640d4b14900f4d903fe5a888a470a
RazviOverflow <razvioverflow@gmail.com>
2023-08-23 21:27:33 +0200

    Added src and exe for PathCchRenameExtension

---
+++ [OC0001] Filesystem/[C0015] Alter Filename Extension/[C0015.008X] PathCchRenameExtension/src/PathCchRenameExtension.cpp
@@ -0,0 +1,17 @@
+// Compiled with: cl /EHsc .\PathCchRenameExtension.cpp Pathcch.lib
+// Alter filename extension with PathCchRenameExtension
+// Adapted from https://learn.microsoft.com/en-us/windows/win32/api/shlwapi/nf-shlwapi-pathaddextensiona
+#include <Windows.h>
+#include <pathcch.h>
+#include <iostream>
+
+int main() {
+    wchar_t filePath[40];
+    PCWSTR newExtension = L".dll";
+    wcsncpy(filePath, L"C:\\Users\\User\\Documents\\example.txtasd", 39);
+    PathCchRenameExtension(filePath, 40, newExtension);
+
+    std::wcout << L"Modified Path: " << filePath << std::endl;
+
+    return 0;
+}
