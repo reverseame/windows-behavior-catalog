abd912157f52cda7d8e1c74d1acead14f078027b
RazviOverflow <razvioverflow@gmail.com>
2023-08-23 20:55:21 +0200

    Added src exe for PathAddExtension

---
+++ [OC0001] Filesystem/[C0015] Alter Filename Extension/[C0015.006X] PathAddExtension/src/PathAddExtension.cpp
@@ -0,0 +1,25 @@
+// Compiled with: cl /EHsc .\PathAddExtension.cpp   Shlwapi.lib
+// Alter filename extension with PathAddExtension
+// Adapted from https://learn.microsoft.com/en-us/windows/win32/api/shlwapi/nf-shlwapi-pathaddextensiona
+#include <windows.h>
+#include <iostream>
+#include "Shlwapi.h"
+
+using namespace std;
+void main( void ){
+     // String for path name without file name extension.
+     char buffer_1[MAX_PATH] = "file";
+     char *lpStr1;
+     lpStr1 = buffer_1;
+
+     // String for extension name.
+     char F_Ext[MAX_PATH] = ".dll";
+     char *lpStr3;
+     lpStr3 = F_Ext;
+
+     // Path 1 without the file name extension.
+     cout << "The original path string 1 is  " << lpStr1 << endl;
+
+     int ret_1 = PathAddExtension(lpStr1,lpStr3);
+     cout << "The modified path string 1 is  " << lpStr1 << endl;
+}
