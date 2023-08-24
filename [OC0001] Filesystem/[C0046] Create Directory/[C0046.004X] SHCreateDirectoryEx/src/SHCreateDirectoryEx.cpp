da97d3f4b2e13fe41eec0d6fa27badd2fcdf9505
RazviOverflow <razvioverflow@gmail.com>
2023-08-24 17:05:06 +0200

    Added src and exe for SHCreateDirectoryEx

---
+++ [OC0001] Filesystem/[C0046] Create Directory/[C0046.004X] SHCreateDirectoryEx/src/SHCreateDirectoryEx.cpp
@@ -0,0 +1,15 @@
+// Compiled with: cl /EHsc .\SHCreateDirectoryEx.cpp Shell32.lib
+// Create directory with SHCreateDirectoryEx
+#include <iostream>
+#include <Shlobj.h>
+
+int main() {
+    HRESULT result = SHCreateDirectoryEx(NULL, "C:\\ExampleDirectory", NULL);
+    if (SUCCEEDED(result)) {
+        std::cout << "Directory created successfully." << std::endl;
+    } else {
+        std::cerr << "Failed to create directory. HRESULT: " << result << std::endl;
+    }
+
+    return 0;
+}
