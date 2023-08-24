0f74bb47aa01ac72789b2cbfa74289d4d6d92fb7
RazviOverflow <razvioverflow@gmail.com>
2023-08-24 18:06:34 +0200

    Added src and exe for GetFileAttributes

---
+++ [OC0001] Filesystem/[C0049] Get File Attributes/[C0049.001X] GetFileAttributes/src/GetFileAttributes.cpp
@@ -0,0 +1,21 @@
+// Compiled with: cl /EHsc .\GetFileAttributes.cpp
+// Get file attributes with GetFileAttributes
+#include <iostream>
+#include <Windows.h>
+
+int main() {
+    const char* filePath = "C:\\Windows\\regedit.exe";
+
+    DWORD fileAttributes = GetFileAttributes(filePath);
+    if (fileAttributes != INVALID_FILE_ATTRIBUTES) {
+        if (fileAttributes & FILE_ATTRIBUTE_DIRECTORY) {
+            std::cout << "File is a directory" << std::endl;
+        } else {
+            std::cout << "File is not a directory" << std::endl;
+        }
+    } else {
+        std::cerr << "Failed to get file attributes. Error code: " << GetLastError() << std::endl;
+    }
+
+    return 0;
+}
