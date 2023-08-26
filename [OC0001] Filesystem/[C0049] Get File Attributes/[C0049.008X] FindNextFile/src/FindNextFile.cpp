0732e0307ec0b51f67ef5807fbea80bcb1d380b0
RazviOverflow <razvioverflow@gmail.com>
2023-08-26 08:57:00 +0100

    Added src and exe for FindNextFile

---
+++ [OC0001] Filesystem/[C0049] Get File Attributes/[C0049.008X] FindNextFile/src/FindNextFile.cpp
@@ -0,0 +1,31 @@
+// Compiled with: cl /EHsc .\FindNextFile.cpp
+// Get file attributes with FindNextFile
+#include <Windows.h>
+#include <iostream>
+#include <string>
+
+int main() {
+    WIN32_FIND_DATAA findFileData;
+    HANDLE hFind = FindFirstFile("C:\\Windows\\*.exe", &findFileData);
+
+    if (hFind == INVALID_HANDLE_VALUE) {
+        std::cerr << "FindFirstFileA failed, error: " << GetLastError() << std::endl;
+        return 1;
+    }
+  
+    if (FindNextFile(hFind, &findFileData) != 0){
+        if (!(findFileData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)) {
+            std::cout << "File: " << findFileData.cFileName << std::endl;
+        } else {
+            std::cout << "Directory: " << findFileData.cFileName << std::endl;
+        }
+    } else {
+        std::cerr << "FindNextFileA failed, error: " << GetLastError() << std::endl;
+        FindClose(hFind);
+        return 1;
+    }
+
+    FindClose(hFind);
+
+    return 0;
+}
