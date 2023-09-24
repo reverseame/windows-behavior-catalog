5ba9293b998cdf0d5139e67dcd44caf3b6aa7de7
RazviOverflow <razvioverflow@gmail.com>
2023-09-24 17:51:47 +0200

    Updated code

--- [OC0006] Communication/[C0001] Socket Communication/[C0001.006] Receive Data/src/socket_recv.cpp
+++ [OC0006] Communication/[C0001] Socket Communication/[C0001.006] Receive Data/src/socket_recv.cpp
@@ -10,14 +10,14 @@ int main() {
     WSADATA wsaData;
     int iResult = WSAStartup(MAKEWORD(2,2), &wsaData);
     if (iResult != 0) {
-        std::cerr << "WSAStartup failed with error: " << iResult << std::endl;
+        printf("WSAStartup failed with error: %d", iResult);
         return 1;
     }
 
     // Create a socket
     SOCKET ConnectSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
     if (ConnectSocket == INVALID_SOCKET) {
-        std::cerr << "socket failed with error: " << WSAGetLastError() << std::endl;
+        printf("socket failed with error: %d", WSAGetLastError());
         WSACleanup();
         return 1;
     }
@@ -25,13 +25,14 @@ int main() {
     // Define the server address
     SOCKADDR_IN serverAddr;
     serverAddr.sin_family = AF_INET;
-    serverAddr.sin_addr.s_addr = inet_addr("1.1.1.1"); // Replace with the IP address of the server you want to connect to
+    // 54.237.103.220 -> https://httpbin.org/ (from https://stackoverflow.com/a/9770981)
+    serverAddr.sin_addr.s_addr = inet_addr("54.237.103.220"); // Replace with the IP address of the server you want to connect to
     serverAddr.sin_port = htons(80); // Replace with the port number of the server
 
     // Connect to the server
     iResult = connect(ConnectSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr));
     if (iResult == SOCKET_ERROR) {
-        std::cerr << "connect failed with error: " << WSAGetLastError() << std::endl;
+        printf("connect failed with error: %d", WSAGetLastError());
         closesocket(ConnectSocket);
         WSACleanup();
         return 1;
@@ -40,10 +41,14 @@ int main() {
     char recvbuf[DEFAULT_BUFLEN];
     int recvbuflen = DEFAULT_BUFLEN;
 
+    // Send something so the server echoes it back
+    char* sendData = "Hello, server!";
+    iResult = send(ConnectSocket, sendData, strlen(sendData), 0);
+
     // Receive one chunk of data
     iResult = recv(ConnectSocket, recvbuf, recvbuflen, 0);
     if ( iResult > 0 )
-        printf("Bytes received: %d\n", iResult);
+        printf("Bytes received: %d\nMessage: %s", iResult, recvbuf);
     else if ( iResult == 0 )
         printf("Connection closed\n");
     else
